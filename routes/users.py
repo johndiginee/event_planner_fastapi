183b052ae41ab87d782ab4043b426e58a7ce2414
John Diginee <Johndbizz@gmail.com>
2023-05-16 18:19:34 +0100

    Implement sign in route

--- routes/users.py
+++ routes/users.py
@@ -18,3 +18,19 @@ async def sign_new_user(data: NewUser) -> dict:
     return {
         "message": "User successfully registered!"
     }
+
+@user_router.post("/signup")
+async def sign_new_in(user: UserSignIn) -> dict:
+    if users[user.email] not in users:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User does not exist"
+        )
+    if users[user.email].password != user.password:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Wrong credentials passed"
+        )
+    return {
+        "message": "User signed in successfully"
+    }
